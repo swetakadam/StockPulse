f1b013a262a0f8621bb6c402f2115cd16d4bfa43
Sweta Kadam <sweta.kadam23@yahoo.com>
2026-03-03 22:57:13 -0800

    Initial Commit

---
+++ StockPulse/StockPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockPulseApp.swift
+//  StockPulse
+//
+//  Created by Sweta Kadam on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct StockPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StockPulseTests/StockPulseTests.swift
@@ -0,0 +1,16 @@
+//
+//  StockPulseTests.swift
+//  StockPulseTests
+//
+//  Created by Sweta Kadam on 3/3/26.
+//
+
+import Testing
+
+struct StockPulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
